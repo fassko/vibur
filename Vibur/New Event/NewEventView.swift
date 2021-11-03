125512795205263aeba1f44b32ce3ca1a982397c
Kristaps Grinbergs <fassko@gmail.com>
2021-11-03 23:32:13 +0200

    Styling form

--- Vibur/New Event/NewEventView.swift
+++ Vibur/New Event/NewEventView.swift
@@ -28,41 +28,96 @@ struct NewEventView: View {
     viewModel = NewEventViewModel(pleasant: pleasant, context: context)
   }
   
+  private let textFieldSpacing: CGFloat = 7
+  
   var body: some View {
     NavigationView {
-      Form {
-        Section {
-          Group {
-          Text("What was the experience?")
-          TextField("Your experience", text: $viewModel.experience)
-            .focused($focusedField, equals: .experience)
-            .onAppear {
-              focusedField = .experience
-            }
+      ScrollView {
+        VStack(alignment: .leading, spacing: 20) {
+          
+          VStack(alignment: .leading, spacing: textFieldSpacing) {
+            Text("What was the experience?")
+              .font(.subheadline)
+              .onTapGesture {
+                focusedField = .experience
+              }
+                        
+            TextField("", text: $viewModel.experience)
+              .textFieldStyle(RoundedTextFieldStyle())
+              .focused($focusedField, equals: .experience)
           }
           
-          Toggle("Were you aware of the \(viewModel.pleasant ? "pleasant" : "unpleasant") feelings while the event was happening?", isOn: $viewModel.aware)
-            .padding(.top)
+          Toggle(isOn: $viewModel.aware) {
+            Text("Were you aware of the \(viewModel.pleasant ? "pleasant" : "unpleasant") feelings while the event was happening?")
+              .font(.subheadline)
+          }
           
-          Text("How did your body feel, in detail, during this experience?")
-            .padding(.top)
-          TextField("", text: $viewModel.feelings)
+          VStack(alignment: .leading, spacing: textFieldSpacing) {
+            Text("How did your body feel, in detail, during this experience?")
+              .font(.subheadline)
+              .onTapGesture {
+                focusedField = .feelings
+              }
+            
+            TextField("", text: $viewModel.feelings)
+              .textFieldStyle(RoundedTextFieldStyle())
+              .focused($focusedField, equals: .feelings)
+          }
+
+          VStack(alignment: .leading, spacing: textFieldSpacing) {
+            Text("What moods and emotions were present?")
+              .font(.subheadline)
+              .onTapGesture {
+                focusedField = .moods
+              }
+            
+            TextField("", text: $viewModel.moods)
+              .textFieldStyle(RoundedTextFieldStyle())
+              .focused($focusedField, equals: .moods)
+          }
           
-          Text("What moods and emotions were present?")
-          TextField("", text: $viewModel.moods)
+          VStack(alignment: .leading, spacing: textFieldSpacing) {
+            Text("What thoughts were present?")
+              .font(.subheadline)
+              .onTapGesture {
+                focusedField = .thoughts
+              }
+            
+            TextField("", text: $viewModel.thoughts)
+              .textFieldStyle(RoundedTextFieldStyle())
+              .focused($focusedField, equals: .thoughts)
+          }
           
-          Text("What thoughts were present?")
-            .padding(.top)
-          TextField("", text: $viewModel.thoughts)
+          VStack(alignment: .leading, spacing: textFieldSpacing) {
+            Text("What thoughts are here as you write this down now?")
+              .font(.subheadline)
+              .onTapGesture {
+                focusedField = .thoughtsWriting
+              }
+            
+            TextField("", text: $viewModel.thoughtsWriting)
+              .textFieldStyle(RoundedTextFieldStyle())
+              .focused($focusedField, equals: .thoughtsWriting)
+          }
           
-          Text("What thoughts are here as you write this down now?")
-            .padding(.top)
-          TextField("", text: $viewModel.thoughtsWriting)
-        }
-        
-        Button("Save", action: save)
+          Button(action: save) {
+            HStack {
+              Spacer()
+              Text("Save")
+              Spacer()
+            }
+          }
           .keyboardShortcut(.defaultAction)
           .disabled(viewModel.saveButtonDisabled)
+          .buttonStyle(.borderedProminent)
+          .controlSize(.large)
+        }
+        .padding()
+      }
+      .onAppear {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+          focusedField = .experience
+        }
       }
       .navigationTitle("New \(viewModel.pleasant ? "Pleasant" : "Unpleasant") Event")
       .toolbar {
@@ -88,9 +143,34 @@ struct NewEventView: View {
   }
 }
 
+
+struct RoundedTextFieldStyle: TextFieldStyle {
+  func _body(configuration: TextField<Self._Label>) -> some View {
+    configuration
+      .padding(10)
+      .background(
+        RoundedRectangle(cornerRadius: 10, style: .continuous)
+          .stroke(.tertiary, lineWidth: 1)
+      )
+  }
+}
+
 struct NewEventView_Previews: PreviewProvider {
   static var previews: some View {
-    NewEventView(pleasant: true, context: PersistenceController.shared.container.viewContext)
-      .navigationViewStyle(StackNavigationViewStyle())
+    Group {
+      NewEventView(pleasant: true, context: PersistenceController.shared.container.viewContext)
+        .previewDevice("iPhone 13 Pro")
+        .navigationViewStyle(StackNavigationViewStyle())
+      
+      NewEventView(pleasant: true, context: PersistenceController.shared.container.viewContext)
+        .preferredColorScheme(.dark)
+        .previewDevice("iPhone 13 Pro")
+        .navigationViewStyle(StackNavigationViewStyle())
+      
+      NewEventView(pleasant: true, context: PersistenceController.shared.container.viewContext)
+        .previewDevice("iPad Pro (11-inch) (3rd generation)")
+        .navigationViewStyle(StackNavigationViewStyle())
+.previewInterfaceOrientation(.landscapeLeft)
+    }
   }
 }
